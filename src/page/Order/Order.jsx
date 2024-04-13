3b798819fae411f9721da2d96cc0029bd66da0a2
AknietTt <119100210+AknietTt@users.noreply.github.com>
2024-04-13 18:13:48 +0500

    test

--- src/page/Order/Order.jsx
+++ src/page/Order/Order.jsx
@@ -1,12 +1,22 @@
-import React, { useEffect } from 'react'
-
+import React, { useEffect, useState } from 'react';
 
 export default function Order() {
+    const [jsonData, setJsonData] = useState('');
 
     useEffect(() => {
-        console.log(window.Telegram.WebApp);
+        // Проверяем, что объект существует
+        if (window.Telegram && window.Telegram.WebApp && window.Telegram.WebApp.initData) {
+            // Преобразуем объект в JSON строку с отступами
+            const formattedData = JSON.stringify(window.Telegram.WebApp.initData, null, 2);
+            // Сохраняем отформатированную строку в состоянии
+            setJsonData(formattedData);
+        }
     }, []);
-  return (
-    <div>Order</div>
-  )
+
+    return (
+        <div>
+            <div>Order</div>
+            <pre>{jsonData}</pre> {/* Отображаем JSON внутри тега <pre>, чтобы сохранить форматирование */}
+        </div>
+    );
 }
